e0d25d81f01f0661def945db9340d02d07dac589
Aldo Villegas <aldoaccs@yahoo.com.mx>
2022-07-27 15:33:11 -0700

    hotfis/ sintax and use of correct tags

--- README.md
+++ README.md
@@ -2,15 +2,6 @@
 
 ### Descripción general
 
-* Figma
-* Imágenes
+En este proyecto se utilizó JavaScript para crear interacción con la pagina, también se utilizaron técnicas aprendidas con anterioridad como las propiedades flex y los grids.
 
-**Figma**
-
-* [Enlace al proyecto en Figma](https://www.figma.com/file/LDMgqWesKpQkIwhOfEBuTS/WEB%2C-Sprint-5%3A-Around-The-U.S.-%7C-desktop-%2B-mobile?node-id=0%3A1)
-
-**Imágenes**
-
-A la hora de trabajar, debes optar por exportar las imágenes directamente desde Figma; te recomendamos hacerlo para practicar más. No olvides optimizarlas [aquí](https://tinypng.com/), para que tu proyecto cargue más rápido. 
-
-¡Esperamos que te diviertas con JavaScript!
+Enlace para la publicación de la pagina:  https://dreikalcc.github.io/web_project_4_esp/
--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-let person = document.querySelector(".profile__name");
-let desc = document.querySelector(".profile__description");
+const person = document.querySelector(".profile__name");
+const desc = document.querySelector(".profile__description");
 
-let editButton = document.querySelector('.profile__edit-button');
+const editButton = document.querySelector('.profile__edit-button');
 //let addButton = document.querySelector('.profile__add-button');
-let closeButton = document.querySelector('.edit__close')
-let likeButton = document.querySelectorAll(".element__like")
-let submitButton = document.querySelector(".input__submit-btn")
+const closeButton = document.querySelector('.edit__close')
+//const likeButton = document.querySelectorAll(".element__like")
+const submitButton = document.querySelector(".input__submit-btn")
 
-let profileEditor = document.querySelector(".edit")
+const profileEditor = document.querySelector(".edit")
 
 
 function editProfile (){
-    profileEditor.classList.toggle("edit__active");
+  profileEditor.classList.toggle("edit__active");
 }
 
-function liking (){
-    likeButton.classList.toggle("element__liked");
-}
+//function liking (){
+//  likeButton.classList.toggle("element__liked");
+//}
 
 function profileData (){
-    let newName = document.querySelector(".input__name");
-    let newDesc = document.querySelector(".input__description");
+  let newName = document.querySelector(".input__name");
+  let newDesc = document.querySelector(".input__description");
 
-    editProfile();
-    
-    person.textContent = newName.value;
-    desc.textContent = newDesc.value;
+  editProfile();
+
+  person.textContent = newName.value;
+  desc.textContent = newDesc.value;
 }
 
 
@@ -34,4 +34,4 @@ function profileData (){
 editButton.addEventListener("click", editProfile);
 closeButton.addEventListener("click", editProfile);
 submitButton.addEventListener("click", profileData);
-likeButton.addEventListener("click", liking);
+//likeButton.addEventListener("click", liking);
