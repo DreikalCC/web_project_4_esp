93357d7d045675d2e4a80c17e18e774f1a178aaa
Aldo Villegas <aldoaccs@yahoo.com.mx>
2022-09-26 16:12:30 -0700

    test

---
+++ scripts/Popup.js
@@ -0,0 +1,34 @@
+import { Card } from "./Card.js";
+
+export default class Popup {
+  constructor(popupSelector, card._viewTheCard) {
+    this._container = document.querySelector(popupSelector);
+  }
+
+  open () {
+    this._container.classList.add(`${popupSelector}_active`);
+  }
+
+  close () {
+    this._container.classList.remove(`${popupSelector}_active`);
+  }
+
+  _handleEscClose () {
+    document.addEventListener("keydown", function(evt){
+      if (evt.key == "Escape" && this._container.classList.contains(`${popupSelector}_active`)){
+        this.close();
+      }
+    })
+  }
+
+  setEvenListeners () {
+    const closeButton = document.querySelector('.${popupSelector}__close');
+    closeButton.addEventListener("click", this.close);
+
+    document.addEventListener("click", function(evt){
+      if ((evt.target.closest(".${popupSelector}__overlay")) && this._container.classList.contains(`${popupSelector}_active`)){
+        this.close();
+      }
+    })
+  }
+}
